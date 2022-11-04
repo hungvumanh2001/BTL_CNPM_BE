dd214917c1d8398112d7ddc03d8d2dbc976c8bf1
hung.vm.dev <hungvumanh2001@gmail.com>
2022-11-04 21:07:46 +0700

    fix receipt repository

--- src/main/java/com/example/cnpm/repository/ReceiptRepository.java
+++ src/main/java/com/example/cnpm/repository/ReceiptRepository.java
@@ -2,11 +2,13 @@ package com.example.cnpm.repository;
 
 import com.example.cnpm.model.Receipt;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface ReceiptRepository extends JpaRepository<Receipt, Long> {
+    @Query("select u from Receipt u where u.user.id = ?1")
     List<Receipt> findAllByUserId(Long userId);
 }
