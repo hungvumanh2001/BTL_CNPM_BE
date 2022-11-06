c0f7a965f2a6e64598b07e9a3bd220b99370c8fa
DuyHung2811 <112716865+DuyHung2811@users.noreply.github.com>
2022-11-06 18:00:20 +0700

    duy hung them

---
+++ src/main/java/com/example/cnpm/service/CategoryService.java
@@ -0,0 +1,9 @@
+package com.example.cnpm.service;
+
+import com.example.cnpm.model.Category;
+
+import java.util.List;
+
+public interface CategoryService {
+    List<Category> findAll();
+}
--- src/main/java/com/example/cnpm/service/ProductService.java
+++ src/main/java/com/example/cnpm/service/ProductService.java
@@ -3,9 +3,7 @@ package com.example.cnpm.service;
 import com.example.cnpm.model.Product;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.stereotype.Service;
 
-@Service
 public interface ProductService {
     Page<Product> findAll(Pageable pageable);
     Page<Product> findAllByTitleContains(Pageable pageable, String key);
