95feff87dad1ab14ee3ee0167aab656a29e700c2
Văn cao <101122902+VanCao21@users.noreply.github.com>
2022-11-09 22:06:35 +0700

    api get all user

--- src/main/java/com/example/cnpm/controller/UserController.java
+++ src/main/java/com/example/cnpm/controller/UserController.java
@@ -41,6 +41,12 @@ public class UserController {
 
     @Autowired
     private PasswordEncoder passwordEncoder;
+    //lấy tất cả các tài khoản ra
+    @GetMapping("/all")
+    public ResponseEntity<Iterable<User>> showAllUser() {
+        Iterable<User> users = userService.findAll();
+        return new ResponseEntity<>(users, HttpStatus.OK);
+    }
     @GetMapping("/search-by-username")
     public ResponseEntity<Iterable<User>> findAllByUserConaining(@RequestParam String name)
     {
