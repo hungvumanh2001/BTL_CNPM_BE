b04245aad64bfb4aebb8b6e9bd9d5b8d09444a2b
hung.vm.dev <hungvumanh2001@gmail.com>
2022-11-13 12:07:11 +0700

    fix SecurityConfig

--- src/main/java/com/example/cnpm/security/SecurityConfig.java
+++ src/main/java/com/example/cnpm/security/SecurityConfig.java
@@ -72,9 +72,9 @@ public class SecurityConfig  extends WebSecurityConfigurerAdapter {
         http.csrf().ignoringAntMatchers("/**");
         http.httpBasic().authenticationEntryPoint(restServicesEntryPoint());
         http.authorizeRequests()
-                .antMatchers("/login","/user/**","/categories/**","/register","/product/**","/cart/**","/receipt/**").permitAll()
-//                .antMatchers("/user/**","/receipt/**","/receipt-details/**").access("hasRole('ROLE_USER')")
-//                .antMatchers("/admin/**").access("hasRole('ROLE_ADMIN')")
+                .antMatchers("/login","/user/**","/categories/**","/register","/products/**","/carts/**").permitAll()
+                .antMatchers("/user/**","/receipt/**","/receipt-details/**").access("hasRole('ROLE_USER')")
+                .antMatchers("/admin/**").access("hasRole('ROLE_ADMIN')")
 //                .antMatchers(HttpMethod.GET
 //                        ).access("hasRole('ROLE_USER') or hasRole('ROLE_ADMIN')")
 //                .antMatchers(HttpMethod.DELETE, "/categories",
